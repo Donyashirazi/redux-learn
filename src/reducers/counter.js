995a7d8ca5f651e8a314826929f175c172c0a7a5
Donya Shirazi <donya_rezaii97@yahoo.com>
2023-02-11 15:04:29 +0330

    first commit

---
+++ src/reducers/counter.js
@@ -0,0 +1,13 @@
+import { increment } from '../actions';
+
+const CounterReducer = (state = 0, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+			return state + action.payload;
+		case 'DECREMENT':
+			return state - 1;
+		default:
+			return state;
+	}
+};
+export default CounterReducer;
